c0385923cf4e718eb0b6c388edcc60cc1cce581c
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-03-25 16:08:50 +0000

    build(npm): copied code from hmpo-form-wizard/example project

---
+++ app.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const session = require('express-session');
+const hmpoLogger = require('hmpo-logger');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const i18n = require('hmpo-i18n');
+const bodyParser = require('body-parser');
+const hmpoComponents = require('hmpo-components');
+const config = require('./config');
+
+const logger = hmpoLogger.config(config.logs).get();
+
+const app = express();
+
+// log access requests
+app.use(hmpoLogger.middleware());
+
+// add routing for static assets
+app.use('/public', express.static(path.resolve(__dirname, 'public')));
+app.use('/public/images', express.static(path.resolve(__dirname, 'node_modules', 'hmpo-components', 'assets', 'images')));
+app.use('/public', express.static(path.resolve(__dirname, 'node_modules', 'govuk-frontend', 'govuk', 'assets')));
+
+
+// cookies and sessions (redis or elasticache should be used in a prod env)
+app.use(cookieParser());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
+
+
+// templating engine (turn on caching and turn off watching in prod env)
+app.set('view engine', 'html');
+const nunjucksEnv = nunjucks.configure([
+  path.resolve(__dirname, 'views'),
+  path.resolve(__dirname, 'node_modules', 'hmpo-components', 'components'),
+  path.resolve(__dirname, 'node_modules', 'govuk-frontend')
+], {
+  express: app,
+  dev: true,
+  noCache: true,
+  watch: true
+});
+
+// localisation support (supports setting language with query parameters and storing in a cookie)
+i18n.middleware(app, {
+  query: 'lang',
+  allowedLangs: [ 'en', 'cy'],
+  cookie: { name: 'lang' },
+  watch: true,
+  baseDir: [
+    path.resolve(__dirname),
+    path.resolve(__dirname, 'node_modules', 'hmpo-components')
+  ]
+});
+
+// initalise hmpo-components
+hmpoComponents.setup(app, nunjucksEnv);
+
+// add locals for templates
+app.use((req, res, next) => {
+  res.locals.assetPath = '/public';
+  res.locals.baseUrl = req.baseUrl;
+  next();
+});
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// wizard routes
+app.use('/', require('./routes/birth'));
+app.use('/birth', require('./routes/birth'));
+
+// file not found handler
+app.use((req, res, next) => {
+  res.status(404).render('pages/file-not-found');
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  logger.error('Request error', { req, err });
+
+  // handle session expired
+  if (err.code === 'SESSION_TIMEOUT') {
+    err.template = 'pages/session-expired';
+  }
+
+  // handle errors with automatic redirects
+  if (err.redirect) {
+    return res.redirect(err.redirect);
+  }
+
+  // show error page
+  err.status = err.status || 500;
+  err.template = err.template || 'pages/error';
+  res.status(err.status).render(err.template, { err });
+});
+
+// listen for incoming requests
+app.listen(config.port, () => {
+  logger.info('App listening on http://localhost::port', { port: config.port });
+});
---
+++ routes/birth/fields.js
@@ -0,0 +1,6 @@
+module.exports = {
+  surname: {
+    type: 'text',
+    validate: 'required'
+  }
+}
