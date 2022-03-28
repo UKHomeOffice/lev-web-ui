5101f0aefae64e6829a0f617036e81c8b07071bf
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-03-28 15:51:58 +0100

    build(npm): added cypress

--- cypress/integration/sample/sample.spec.js
+++ cypress/integration/sample/sample.spec.js
@@ -1,6 +1,6 @@
 describe('UI tests', () => {
-    it('Home page should be displayed', () => {
-        cy.visit('/');
-        cy.get('h1').contains('Applicant\'s details');
-    });
+  it('Home page should be displayed', () => {
+    cy.visit('/');
+    cy.get('h1').contains('Applicant\'s details');
+  });
 });
