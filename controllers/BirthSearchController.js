7818d645778e1ea0d75cce5a8566efe8259de0ab
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-04-12 09:22:30 +0100

    refactor(hmpo): date formatted in nunjucks

--- controllers/BirthSearchController.js
+++ controllers/BirthSearchController.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const { Controller } = require('hmpo-form-wizard');
-const moment = require('moment');
 const DateControllerMixin = require('hmpo-components').mixins.Date;
 const DateController = DateControllerMixin(Controller);
 
@@ -11,7 +10,7 @@ class BirthSearchController extends DateController {
       systemNumber: req.form.values['system-number'],
       surname: req.form.values['surname'],
       forenames: req.form.values['forenames'],
-      dob: moment(req.form.values['dob'], 'YYYY-MM-DD').format('DD/MM/YYYY')
+      dob: req.form.values['dob']
     }
 
     req.sessionModel.set('searchTerms', searchTerms);
