ae143d10de8cc3f425abea9c4f1a1f531fd9977d
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-05-11 16:19:59 +0100

    feat(hmpo): changes to config

--- config/index.js
+++ config/index.js
@@ -2,21 +2,12 @@
 
 const packageJson = require('../package.json');
 
-const conf = {
+module.exports = {
   api: {
     protocol: process.env.API_PROTOCOL || 'http',
     host: process.env.API_HOST || 'localhost',
     port: process.env.API_PORT || 8080,
     client: packageJson.name,
     username: process.env.USER || packageJson.name
-  },
-  logs: {
-    console: true,
-    app: false,
-    error: false
-  },
-  port: process.env.PORT || 8001
+  }
 };
-
-
-module.exports = conf;
