3ec1244ac561bc870b65ac3d973a7d3a175fa10e
James Woodward <93145293+JamesWoodwardHMPO@users.noreply.github.com>
2022-05-13 11:20:36 +0100

    Add rejectUnauthorized (#21)
    * refactor(hmpo): refactor headers
    
    * refactor(hmpo): refactor headers
    
    * refactor(hmpo): refactor headers
    
    * build(npm) update dependencies

--- config/index.js
+++ config/index.js
@@ -8,6 +8,7 @@ module.exports = {
     host: process.env.API_HOST || 'localhost',
     port: process.env.API_PORT || 8080,
     client: packageJson.name,
-    username: process.env.USER || packageJson.name
+    username: process.env.USER || packageJson.name,
+    rejectUnauthorized: process.env.LEV_TLS_VERIFY || false
   }
 };
---
+++ controllers/BaseController.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { Controller } = require('hmpo-form-wizard');
+const {api} = require('../config');
+
+class BaseController extends Controller {
+
+  /**
+   * Populate headers for the rest api from the current authenticated user
+   *
+   * @param req
+   * @returns {{"X-Auth-Username": *, "X-Auth-Aud"}}
+   */
+  static getHeaders(req) {
+    const token = req.headers['X-Auth-Token'];
+    const roles = req.headers['X-Auth-Roles'];
+
+    let headers = {
+      'X-Auth-Aud': api.client,
+      'X-Auth-Username': api.username
+    };
+
+    if (token) {
+      headers = {
+        ...headers,
+        'Authorization': `Bearer ${token}`
+      };
+    } else if (roles) {
+      headers = {
+        ...headers,
+        'X-Auth-Roles': roles
+      };
+    }
+
+    return headers;
+  }
+
+  static getOptions(req) {
+    return {
+      headers: this.getHeaders(req),
+      https: {
+        rejectUnauthorized: api.rejectUnauthorized
+      }
+    };
+  }
+}
+
+module.exports = BaseController;
