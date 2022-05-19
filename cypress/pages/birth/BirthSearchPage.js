d6e15d5d66235aa379d77d54c97d05f06c5f1026
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2022-05-19 13:43:27 +0100

    Added birth search tests, supporting fixtures/pages

--- cypress/pages/birth/BirthSearchPage.js
+++ cypress/pages/birth/BirthSearchPage.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const SearchPage = require('../SearchPage');
-
 class BirthSearchPage extends SearchPage {
 
   /**
@@ -19,9 +18,6 @@ class BirthSearchPage extends SearchPage {
     // Has title
     cy.get('h1').contains('Applicant\'s details');
 
-    // Has focus
-    cy.get('#system-number').focus().should('have.focus');
-
     // Has labels
     cy.get('label[for=system-number]').contains('System number from birth certificate');
     cy.get('label[for=surname]').contains('Surname');
@@ -29,6 +25,99 @@ class BirthSearchPage extends SearchPage {
     cy.get('label[for=dob-day]').contains('Day');
     cy.get('label[for=dob-month]').contains('Month');
     cy.get('label[for=dob-year]').contains('Year');
+  }
+
+  static performSearch({
+    systemNumber,
+    surname,
+    forenames,
+    day,
+    month,
+    year
+  }) {
+    this.setText('#system-number', systemNumber);
+    this.setText('#surname', surname);
+    this.setText('#forenames', forenames);
+    this.setText('#dob-day', day);
+    this.setText('#dob-month', month);
+    this.setText('#dob-year', year);
+    cy.get('.button').click();
+  }
+
+  static noSearchCriteria() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter a surname');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter at least one forename');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter a date of birth');
+  }
+
+  static noSystemNumber() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter a number');
+    cy.get('#system-number-hint').should('exist');
+  }
+
+  static invalidLengthSystemNumber() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('The system number should be 9 digits');
+    cy.get('#system-number-hint').should('exist');
+  }
+
+  static noForenames() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter at least one forename');
+  }
+
+  static noSurname() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter a surname');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter at least one forename');
+  }
+
+  static invalidDOBDay() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Enter a day using numbers only');
+    cy.get('#dob-error.govuk-error-message').should('exist');
+  }
+
+  static invalidDOBMonth() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Enter a month using numbers only');
+    cy.get('#dob-error.govuk-error-message').should('exist');
+  }
+
+  static invalidDOBYear() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Enter a year using numbers only');
+    cy.get('#dob-error.govuk-error-message').should('exist');
+  }
+
+  static dobInFuture() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter a date of birth in the past');
+    cy.get('#dob-error.govuk-error-message').should('exist');
+  }
+
+  static dobBeforeRecordsBegan() {
+    cy.get('.error-summary').contains('Fix the following error(s)');
+    cy.get('.govuk-error-summary__list > li').contains('Please enter a date after our records began (1 July 2009)');
+    cy.get('#dob-error.govuk-error-message').should('exist');
+  }
+
+  static searchFormClear() {
+    cy.get('#system-number').should('have.value', '');
+    cy.get('#surname').should('have.value', '');
+    cy.get('#forenames').should('have.value', '');
+    cy.get('#dob').should('have.value', '');
+  }
+
+  static searchFormRetainedValues(record) {
+    cy.get('#system-number').should('have.value', '');
+    cy.get('#surname').should('have.value', `${record.surname}`);
+    cy.get('#forenames').should('have.value', `${record.forenames}`);
+    cy.get('#dob-day').should('have.value', `${record.day}`);
+    cy.get('#dob-month').should('have.value', `${record.month}`);
+    cy.get('#dob-year').should('have.value', `${record.year}`);
   }
 }
 
