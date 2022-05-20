338303f0a634cae8d8d6282d6f95c94f0ac6e125
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-05-20 09:59:54 +0100

    feat(hmpo): decorate record with flags and change levFLag to accept html params

--- controllers/BirthDetailsController.js
+++ controllers/BirthDetailsController.js
@@ -19,11 +19,56 @@ class BirthDetailsController extends BaseController {
 
       const currentRecord = req.sessionModel.get('currentRecord');
 
-      locals.record = searchResults[currentRecord];
+      locals.record = this.processRecord(searchResults[currentRecord]);
       locals.showBackToResults = searchResults.length > 1;
 
       callback(null, locals);
     });
+  }
+
+  processRecord(record) {
+
+    // Status is blocked
+    if (record.status.blocked) {
+      return {
+        ...record,
+        flags: {
+          refer: true
+        }
+      };
+    }
+
+    // Status is not blocked, populate flags
+    const refer =
+      record.status.blocked ||
+      record.status.cancelled ||
+      record.status.potentiallyFictitious ||
+    (
+      record.status.reregistration !== 'None' &&
+      record.status.reregistration !== 'Father added' &&
+      record.status.reregistration !== 'Subsequently married' &&
+      record.status.reregistration !== 'Father modified' &&
+      record.status.reregistration !== 'Replacement registration'
+    ) ||
+    (
+      record.status.marginalNote !== 'None' &&
+      record.status.marginalNote !== 'Court order in place' &&
+      record.status.marginalNote !== 'Court order revoked'
+    );
+
+    return {
+      ...record,
+      flags: {
+        refer,
+        fatherAdded: record.status.reregistration === 'Father added',
+        subsequentlyMarried: record.status.reregistration === 'Subsequently married',
+        fatherModified: record.status.reregistration === 'Father modified',
+        replaced: record.status.reregistration === 'Replacement registration',
+        corrected: record.status.correction && record.status.correction !== 'None',
+        courtOrderInPlace: record.status.marginalNote === 'Court order in place',
+        courtOrderRevoked: record.status.marginalNote === 'Court order revoked'
+      }
+    };
   }
 }
 
