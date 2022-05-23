2e8069bbe4f521cd6cc271de38d87779ebd12eba
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-05-23 09:34:53 +0100

    feat(hmpo): decorate results with flags

--- controllers/BirthDetailsController.js
+++ controllers/BirthDetailsController.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const BaseController = require('./BaseController');
+const BirthController = require('./BirthController');
 const { searchById } = require('../api');
 
-class BirthDetailsController extends BaseController {
+class BirthDetailsController extends BirthController {
   locals(req, res, callback) {
     super.locals(req, res, async (error, locals) => {
       if (error) return callback(error);
@@ -48,51 +48,6 @@ class BirthDetailsController extends BaseController {
         callback(this.pageNotFound());
       }
     });
-  }
-
-  processRecord(record) {
-
-    // Status is blocked
-    if (record.status.blocked) {
-      return {
-        ...record,
-        flags: {
-          refer: true
-        }
-      };
-    }
-
-    // Status is not blocked, populate flags
-    const refer =
-      record.status.blocked ||
-      record.status.cancelled ||
-      record.status.potentiallyFictitious ||
-    (
-      record.status.reregistration !== 'None' &&
-      record.status.reregistration !== 'Father added' &&
-      record.status.reregistration !== 'Subsequently married' &&
-      record.status.reregistration !== 'Father modified' &&
-      record.status.reregistration !== 'Replacement registration'
-    ) ||
-    (
-      record.status.marginalNote !== 'None' &&
-      record.status.marginalNote !== 'Court order in place' &&
-      record.status.marginalNote !== 'Court order revoked'
-    );
-
-    return {
-      ...record,
-      flags: {
-        refer,
-        fatherAdded: record.status.reregistration === 'Father added',
-        subsequentlyMarried: record.status.reregistration === 'Subsequently married',
-        fatherModified: record.status.reregistration === 'Father modified',
-        replaced: record.status.reregistration === 'Replacement registration',
-        corrected: record.status.correction && record.status.correction !== 'None',
-        courtOrderInPlace: record.status.marginalNote === 'Court order in place',
-        courtOrderRevoked: record.status.marginalNote === 'Court order revoked'
-      }
-    };
   }
 }
 
