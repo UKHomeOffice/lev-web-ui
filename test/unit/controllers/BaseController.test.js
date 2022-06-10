cc3323d8ea5bcdbafee020342395559e50bb110f
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-06-10 13:05:36 +0100

    feat(jest): add unit tests

---
+++ test/unit/controllers/BaseController.test.js
@@ -0,0 +1,171 @@
+'use strict';
+
+const BaseController = require('../../../controllers/BaseController');
+const { api } = require('../../../config');
+
+describe('BaseController tests', () => {
+  const controller = new BaseController({ route: '/index' });
+
+  describe('getOptions()', () => {
+    describe('When no token or roles exists in the request headers', () => {
+      it('should return an options object', () => {
+
+        // Arrange
+        const req = { headers: {} };
+        const expected = {
+          headers: {
+            'x-auth-aud': api.client,
+            'x-auth-username': api.username
+          },
+          https: {
+            rejectUnauthorized: api.rejectUnauthorized
+          }
+        };
+
+        // Act
+        const received = controller.getOptions(req);
+
+        // Assert
+        expect(received.headers['x-auth-aud']).toBe(expected.headers['x-auth-aud']);
+        expect(received.headers['x-auth-username']).toBe(expected.headers['x-auth-username']);
+        expect(received.https.rejectUnauthorized).toBe(expected.https.rejectUnauthorized);
+      });
+    });
+
+    describe('When token exists in the request headers', () => {
+      it('should return an options object', () => {
+
+        // Arrange
+        const req = { headers: {
+          'x-auth-token': 'xxxxx'
+        } };
+        const expected = {
+          headers: {
+            'x-auth-aud': api.client,
+            'x-auth-username': api.username,
+            Authorization: 'Bearer xxxxx'
+          },
+          https: {
+            rejectUnauthorized: api.rejectUnauthorized
+          }
+        };
+
+        // Act
+        const received = controller.getOptions(req);
+
+        // Assert
+        expect(received.headers['x-auth-aud']).toBe(expected.headers['x-auth-aud']);
+        expect(received.headers['x-auth-username']).toBe(expected.headers['x-auth-username']);
+        expect(received.headers.Authorization).toBe(expected.headers.Authorization);
+        expect(received.https.rejectUnauthorized).toBe(expected.https.rejectUnauthorized);
+      });
+    });
+    describe('When roles exists in the request headers', () => {
+      it('should return an options object', () => {
+
+        // Arrange
+        const req = { headers: {
+          'x-auth-roles': 'full-details'
+        } };
+        const expected = {
+          headers: {
+            'x-auth-aud': api.client,
+            'x-auth-username': api.username,
+            'x-auth-roles': 'full-details'
+          },
+          https: {
+            rejectUnauthorized: api.rejectUnauthorized
+          }
+        };
+
+        // Act
+        const received = controller.getOptions(req);
+
+        // Assert
+        expect(received.headers['x-auth-aud']).toBe(expected.headers['x-auth-aud']);
+        expect(received.headers['x-auth-username']).toBe(expected.headers['x-auth-username']);
+        expect(received.headers['x-auth-roles']).toBe(expected.headers['x-auth-roles']);
+        expect(received.https.rejectUnauthorized).toBe(expected.https.rejectUnauthorized);
+      });
+    });
+    describe('When both token and roles exists in the request headers', () => {
+      it('should return an options object', () => {
+
+        // Arrange
+        const req = { headers: {
+          'x-auth-token': 'xxxxx',
+          'x-auth-roles': 'full-details'
+        } };
+        const expected = {
+          headers: {
+            'x-auth-aud': api.client,
+            'x-auth-username': api.username,
+            'x-auth-roles': 'full-details',
+            Authorization: 'Bearer xxxxx'
+          },
+          https: {
+            rejectUnauthorized: api.rejectUnauthorized
+          }
+        };
+
+        // Act
+        const received = controller.getOptions(req);
+
+        // Assert
+        expect(received.headers['x-auth-aud']).toBe(expected.headers['x-auth-aud']);
+        expect(received.headers['x-auth-username']).toBe(expected.headers['x-auth-username']);
+        expect(received.headers['x-auth-roles']).toBeUndefined();
+        expect(received.headers.Authorization).toBe(expected.headers.Authorization);
+        expect(received.https.rejectUnauthorized).toBe(expected.https.rejectUnauthorized);
+      });
+    });
+  });
+
+  describe('pageNotFound()', () => {
+    it('should return an error object', () => {
+
+      // Arrange
+      const expected = {
+        code: 'PAGE_NOT_FOUND',
+        status: 404,
+        template: 'errors/page-not-found'
+      };
+
+      // Act
+      const err = controller.pageNotFound();
+
+      // Assert
+      expect(err.code).toBe(expected.code);
+      expect(err.status).toBe(expected.status);
+      expect(err.template).toBe(expected.template);
+    });
+  });
+
+  describe('hasRole()', () => {
+    it('should return false when the given role is not present in the request headers', () => {
+
+      // Arrange
+      const req = { headers: { 'x-auth-roles': [] } };
+      const role = 'full-details';
+
+      // Act
+      const result = controller.hasRole(req, role);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+
+    it('should return true when the given role is present in the request headers', () => {
+
+      // Arrange
+      const req = { headers: { 'x-auth-roles': ['full-details'] } };
+      const role = 'full-details';
+
+      // Act
+      const result = controller.hasRole(req, role);
+
+      // Assert
+      expect(result).toBe(true);
+    });
+  });
+});
