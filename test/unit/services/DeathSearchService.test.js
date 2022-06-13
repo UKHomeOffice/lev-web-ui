4d9017b0c4b8e251c8b40e7690d38fa1a4ed77e7
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-06-13 08:11:16 +0100

    feat(jest): add unit tests

---
+++ test/unit/services/DeathSearchService.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const DeathSearchService = require('../../../services/DeathSearchService');
+
+describe('DeathSearchService tests', () => {
+  describe('processRecord()', () => {
+    describe('refer flag should be true', () => {
+      it('if record is blocked', () => {
+
+        // Arrange
+        const record = {
+          status: {
+            blocked: true
+          }
+        };
+
+        // Act
+        const received = DeathSearchService.processRecord(record);
+
+        // Assert
+        expect(received.flags.refer).toBe(true);
+      });
+
+      it('if record marginal note is not "None"', () => {
+
+        // Arrange
+        const record = {
+          status: {
+            marginalNote: 'Marginal'
+          }
+        };
+
+        // Act
+        const received = DeathSearchService.processRecord(record);
+
+        // Assert
+        expect(received.flags.refer).toBe(true);
+      });
+    });
+    describe('corrected flag should be true', () => {
+      it('if record correction is not "None"', () => {
+
+        // Arrange
+        const record = {
+          status: {
+            correction: 'Corrected'
+          }
+        };
+
+        // Act
+        const received = DeathSearchService.processRecord(record);
+
+        // Assert
+        expect(received.flags.corrected).toBe(true);
+      });
+    });
+  });
+});
