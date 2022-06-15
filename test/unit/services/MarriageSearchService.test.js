ba2e7f77f069b72eb880005290b01493732317d2
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-06-15 12:13:46 +0100

    feat(jest): add marriage

---
+++ test/unit/services/MarriageSearchService.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const MarriageSearchService = require('../../../services/MarriageSearchService');
+
+describe('MarriageSearchService tests', () => {
+  describe('processRecord()', () => {
+    describe('refer flag should be true', () => {
+      it('if record is blocked', () => {
+
+        // Arrange
+        const record = {
+          status: {
+            blocked: true
+          }
+        };
+
+        // Act
+        const received = MarriageSearchService.processRecord(record);
+
+        // Assert
+        expect(received.flags.refer).toEqual(true);
+      });
+
+      it('if record marginal note is not "None"', () => {
+
+        // Arrange
+        const record = {
+          status: {
+            marginalNote: 'Marginal'
+          }
+        };
+
+        // Act
+        const received = MarriageSearchService.processRecord(record);
+
+        // Assert
+        expect(received.flags.refer).toEqual(true);
+      });
+    });
+  });
+});
