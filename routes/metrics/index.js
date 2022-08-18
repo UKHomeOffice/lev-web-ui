f9a174cca589d8df136e4ca2fc5dc323a8337e8b
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-08-18 09:29:05 +0100

    refactor(metrics): tidy up register

--- routes/metrics/index.js
+++ routes/metrics/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const promClient = require('prom-client');
+const register = promClient.register;
 const packageJson = require('../../package.json');
-
-let register = new promClient.Registry();
 
 // Set default labels for all metrics
 register.setDefaultLabels({
@@ -12,6 +11,11 @@ register.setDefaultLabels({
 
 const promPrefix = 'lev_web_ui';
 const promMetrics = {};
+
+const metricsRoute = async (req, res) => {
+  res.setHeader('Content-type', register.contentType);
+  res.end(await register.metrics());
+};
 
 const initialiseMetrics = () => {
 
@@ -25,7 +29,6 @@ const initialiseMetrics = () => {
 
   // Counters
   promMetrics.req = new promClient.Counter({
-    registers: [register],
     name: `${promPrefix}_req`,
     help: 'Number of requests'
   });
@@ -33,7 +36,6 @@ const initialiseMetrics = () => {
   // Request types
   reqTypes.forEach(reqType => {
     promMetrics.req[reqType] = new promClient.Counter({
-      registers: [register],
       name: `${promPrefix}_req_${reqType}`,
       help: `Number of ${reqType} requests`
     });
@@ -42,7 +44,6 @@ const initialiseMetrics = () => {
   // Datasets
   dataSets.forEach(dataSet => {
     promMetrics.req[dataSet] = new promClient.Counter({
-      registers: [register],
       name: `${promPrefix}_req_${dataSet}`,
       help: `Number of ${dataSet} requests`
     });
@@ -50,7 +51,6 @@ const initialiseMetrics = () => {
     // Request types per dataset
     reqTypes.forEach(reqType => {
       promMetrics.req[dataSet][reqType] = new promClient.Counter({
-        registers: [register],
         name: `${promPrefix}_req_${dataSet}_${reqType}`,
         help: `Number of ${dataSet} ${reqType} requests`
       });
@@ -62,7 +62,6 @@ const initialiseMetrics = () => {
   // Request types
   reqTypes.forEach(reqType => {
     promMetrics.req[reqType].time = new promClient.Histogram({
-      registers: [register],
       name: `${promPrefix}_req_${reqType}_time`,
       help: `Request time of ${reqType} requests`
     });
@@ -71,7 +70,6 @@ const initialiseMetrics = () => {
   // Datasets
   dataSets.forEach(dataSet => {
     promMetrics.req[dataSet].time = new promClient.Histogram({
-      registers: [register],
       name: `${promPrefix}_req_${dataSet}_time`,
       help: `Request time of ${dataSet} requests`
     });
@@ -79,7 +77,6 @@ const initialiseMetrics = () => {
     // Request types per dataset
     reqTypes.forEach(reqType => {
       promMetrics.req[dataSet][reqType].time = new promClient.Histogram({
-        registers: [register],
         name: `${promPrefix}_req_${dataSet}_${reqType}_time`,
         help: `Request time of ${dataSet} ${reqType} requests`
       });
@@ -90,7 +87,6 @@ const initialiseMetrics = () => {
 
   // Counters
   promMetrics.err = new promClient.Counter({
-    registers: [register],
     name: `${promPrefix}_err`,
     help: 'Number of errors'
   });
@@ -98,7 +94,6 @@ const initialiseMetrics = () => {
   // Request types
   reqTypes.forEach(reqType => {
     promMetrics.err[reqType] = new promClient.Counter({
-      registers: [register],
       name: `${promPrefix}_err_${reqType}`,
       help: `Number of ${reqType} errors`
     });
@@ -107,7 +102,6 @@ const initialiseMetrics = () => {
   // Datasets
   dataSets.forEach(dataSet => {
     promMetrics.err[dataSet] = new promClient.Counter({
-      registers: [register],
       name: `${promPrefix}_err_${dataSet}`,
       help: `Number of ${dataSet} errors`
     });
@@ -115,7 +109,6 @@ const initialiseMetrics = () => {
     // Request types per dataset
     reqTypes.forEach(reqType => {
       promMetrics.err[dataSet][reqType] = new promClient.Counter({
-        registers: [register],
         name: `${promPrefix}_err_${dataSet}_${reqType}`,
         help: `Number of ${dataSet} ${reqType} errors`
       });
@@ -127,7 +120,6 @@ const initialiseMetrics = () => {
   // Request types
   reqTypes.forEach(reqType => {
     promMetrics.err[reqType].time = new promClient.Histogram({
-      registers: [register],
       name: `${promPrefix}_err_${reqType}_time`,
       help: `Request time of ${reqType} errors`
     });
@@ -136,7 +128,6 @@ const initialiseMetrics = () => {
   // Datasets
   dataSets.forEach(dataSet => {
     promMetrics.err[dataSet].time = new promClient.Histogram({
-      registers: [register],
       name: `${promPrefix}_err_${dataSet}_time`,
       help: `Request time of ${dataSet} errors`
     });
@@ -144,7 +135,6 @@ const initialiseMetrics = () => {
     // Request types per dataset
     reqTypes.forEach(reqType => {
       promMetrics.err[dataSet][reqType].time = new promClient.Histogram({
-        registers: [register],
         name: `${promPrefix}_err_${dataSet}_${reqType}_time`,
         help: `Request time of ${dataSet} ${reqType} errors`
       });
@@ -161,26 +151,22 @@ const initialiseGroupMetrics = (groups) => {
 
     // Requests
     promMetrics.req[group] = promMetrics.req[group] || new promClient.Counter({
-      registers: [register],
       name: `${promPrefix}_req_group_${escape(group)}`,
       help: `Number of requests from members of the ${escape(group)} group`
     });
 
     promMetrics.req[group].time = promMetrics.req[group].time || new promClient.Histogram({
-      registers: [register],
       name: `${promPrefix}_req_group_${escape(group)}_time`,
       help: `Request time of requests from members of the ${escape(group)} group`
     });
 
     // Errors
     promMetrics.err[group] = promMetrics.err[group] || new promClient.Counter({
-      registers: [register],
       name: `${promPrefix}_err_group_${escape(group)}`,
       help: `Number of errors from members of the ${escape(group)} group`
     });
 
     promMetrics.err[group].time = promMetrics.err[group].time || new promClient.Histogram({
-      registers: [register],
       name: `${promPrefix}_err_group_${escape(group)}_time`,
       help: `Request time of errors from members of the ${escape(group)} group`
     });
@@ -216,11 +202,6 @@ const incrementRequestMetrics = (reqType, dataSet, groups, duration) => {
 
 const incrementErrorMetrics = (reqType, dataSet, groups, duration) => {
   incrementMetrics('err', reqType, dataSet, groups, duration);
-};
-
-const metricsRoute = async (req, res) => {
-  res.setHeader('Content-type', register.contentType);
-  res.end(await register.metrics());
 };
 
 initialiseMetrics();
