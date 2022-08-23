723f8ec5ac1dd369c946d9af5da30a7110a687f5
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-08-23 11:06:24 +0100

    refactor(jest): added jest.restoreAllMocks()

--- test/unit/metrics.test.js
+++ test/unit/metrics.test.js
@@ -1,102 +1,62 @@
-const {
-  incrementRequestMetrics,
-  getCurrentTimeInMillis,
-  incrementErrorMetrics,
-  promMetrics
-} = require('../../lib/metrics');
+const { incrementRequestMetrics, incrementErrorMetrics, promMetrics } = require('../../lib/metrics');
 
 describe('metric tests', () => {
+  const reqTypes = ['lookup', 'search'];
   const dataSets = ['birth', 'death', 'marriage', 'partnership'];
 
   describe('req counter', () => {
-    const reqCounter = jest.spyOn(promMetrics.req, 'inc');
-    const lookUpCounter = jest.spyOn(promMetrics.req.lookup, 'inc');
-    const searchCounter = jest.spyOn(promMetrics.req.search, 'inc');
+    reqTypes.forEach(reqType => {
+      describe(`${reqType} counter`, () => {
+        dataSets.forEach(dataSet => {
+          test(`${dataSet} dataset counter incremented`, () => {
 
-    describe('lookups', () => {
-      dataSets.forEach(dataSet => {
-        test(`${dataSet} dataset counter incremented`, () => {
+            // Arrange
+            const reqCounter = jest.spyOn(promMetrics.req, 'inc');
+            const reqTypeCounter = jest.spyOn(promMetrics.req[reqType], 'inc');
+            const datasetCounter = jest.spyOn(promMetrics.req[dataSet], 'inc');
+            const datasetReqTypeCounter = jest.spyOn(promMetrics.req[dataSet][reqType], 'inc');
 
-          // Arrange
-          const datasetCounter = jest.spyOn(promMetrics.req[dataSet], 'inc');
+            // Act
+            incrementRequestMetrics(reqType, dataSet, ['test-group'], 1);
 
-          // Act
-          incrementRequestMetrics('lookup', dataSet, ['test-group'], 1);
+            // Assert
+            expect(reqCounter).toHaveBeenCalledTimes(1);
+            expect(reqTypeCounter).toHaveBeenCalledTimes(1);
+            expect(datasetCounter).toHaveBeenCalledTimes(1);
+            expect(datasetReqTypeCounter).toHaveBeenCalledTimes(1);
 
-          // Assert
-          expect(datasetCounter).toHaveBeenCalledTimes(1);
-          expect(reqCounter).toHaveBeenCalledTimes(1);
-          expect(lookUpCounter).toHaveBeenCalledTimes(1);
-        });
-      });
-    });
-
-    describe('searches', () => {
-      dataSets.forEach(dataSet => {
-        test(`${dataSet} dataset counter incremented`, () => {
-
-          // Arrange
-          const datasetCounter = jest.spyOn(promMetrics.req[dataSet], 'inc');
-
-          // Act
-          incrementRequestMetrics('search', dataSet, ['test-group'], 1);
-
-          // Assert
-          expect(datasetCounter).toHaveBeenCalledTimes(1);
-          expect(reqCounter).toHaveBeenCalledTimes(1);
-          expect(searchCounter).toHaveBeenCalledTimes(1);
+            jest.restoreAllMocks();
+          });
         });
       });
     });
   });
 
   describe('err counter', () => {
-    const reqCounter = jest.spyOn(promMetrics.err, 'inc');
-    const lookUpCounter = jest.spyOn(promMetrics.err.lookup, 'inc');
-    const searchCounter = jest.spyOn(promMetrics.err.search, 'inc');
+    reqTypes.forEach(reqType => {
+      describe(`${reqType} counter`, () => {
+        dataSets.forEach(dataSet => {
+          test(`${dataSet} dataset counter incremented`, () => {
 
-    describe('lookups', () => {
-      dataSets.forEach(dataSet => {
-        test(`${dataSet} dataset counter incremented`, () => {
+            // Arrange
+            const reqCounter = jest.spyOn(promMetrics.err, 'inc');
+            const reqTypeCounter = jest.spyOn(promMetrics.err[reqType], 'inc');
+            const datasetCounter = jest.spyOn(promMetrics.err[dataSet], 'inc');
+            const datasetReqTypeCounter = jest.spyOn(promMetrics.err[dataSet][reqType], 'inc');
 
-          // Arrange
-          const datasetCounter = jest.spyOn(promMetrics.err[dataSet], 'inc');
+            // Act
+            incrementErrorMetrics(reqType, dataSet, ['test-group'], 1);
 
-          // Act
-          incrementErrorMetrics('lookup', dataSet, ['test-group'], 1);
+            // Assert
+            expect(reqCounter).toHaveBeenCalledTimes(1);
+            expect(reqTypeCounter).toHaveBeenCalledTimes(1);
+            expect(datasetCounter).toHaveBeenCalledTimes(1);
+            expect(datasetReqTypeCounter).toHaveBeenCalledTimes(1);
 
-          // Assert
-          expect(datasetCounter).toHaveBeenCalledTimes(1);
-          expect(reqCounter).toHaveBeenCalledTimes(1);
-          expect(lookUpCounter).toHaveBeenCalledTimes(1);
+            jest.restoreAllMocks();
+          });
         });
       });
-    });
-
-    describe('searches', () => {
-      dataSets.forEach(dataSet => {
-        test(`${dataSet} dataset counter incremented`, () => {
-
-          // Arrange
-          const datasetCounter = jest.spyOn(promMetrics.err[dataSet], 'inc');
-
-          // Act
-          incrementErrorMetrics('search', dataSet, ['test-group'], 1);
-
-          // Assert
-          expect(datasetCounter).toHaveBeenCalledTimes(1);
-          expect(reqCounter).toHaveBeenCalledTimes(1);
-          expect(searchCounter).toHaveBeenCalledTimes(1);
-        });
-      });
-    });
-  });
-
-  describe('get time function', () => {
-    test('date/time is represented as a number', () => {
-
-      // Assert
-      expect(typeof getCurrentTimeInMillis()).toBe('number');
     });
   });
 });
