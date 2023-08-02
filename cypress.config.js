7e76b61e8cb03815cd24e864560bc751daa5c5c2
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2023-08-02 18:11:50 +0100

    added html report with screenshots to be generated

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+
       // Populate the environment from process.env
       config.env.env = process.env.ENV || 'local';
       config.env.e2e = config.env.env !== 'local';
@@ -12,13 +14,14 @@ module.exports = defineConfig({
       // configure cypress-keycloak
       config.env.keycloak = {
         root: process.env.KEYCLOAK_URL,
-        realm: 'lev_dev',
+        realm: 'lev',
         username: process.env.TEST_USERNAME,
         password: process.env.TEST_PASSWORD,
         // eslint-disable-next-line camelcase
         client_id: 'lev-web-ui',
         // eslint-disable-next-line camelcase
-        path_prefix: ''
+        redirect_uri: process.env.TEST_URL,
+        path_prefix: 'auth'
       };
 
       // configure cypress-axe
@@ -36,6 +39,15 @@ module.exports = defineConfig({
       });
 
       return config;
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      json: false,
+      charts: true,
+      reportFilename: 'Cypress Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: true
     },
     testIsolation: false,
     baseUrl: 'http://localhost:8001',
