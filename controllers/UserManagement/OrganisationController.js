c46005aa2a98066d5afcb919c75af945a07e72cc
nayhallbaqi <nayhall.baqai@digital.homeoffice.gov.uk>
2024-05-30 12:13:59 +0100

    Teams view for organisation

--- controllers/UserManagement/OrganisationController.js
+++ controllers/UserManagement/OrganisationController.js
@@ -8,7 +8,22 @@ class OrganisationController extends BaseController {
         ...this.getOptions(req),
         url: `/admin/organisations/${req.params.id}`
       });
+      let teamResults = await orgLookup({
+        ...this.getOptions(req),
+        url: `/admin/organisations/${req.params.id}/teams`
+      });
+
+      teamResults.sort((t1, t2) => {
+        const team1 = t1.name.toLowerCase();
+        const team2 = t2.name.toLowerCase();
+        if (team1 > team2) { return 1; }
+        if (team1 < team2) { return -1; }
+        return 0;
+      });
+      
       req.sessionModel.set('orgResults', searchResults);
+      req.sessionModel.set('teamResults', teamResults);
+
       next();
     } catch (err) {
       err.template = 'errors/organisation-error';
@@ -20,6 +35,7 @@ class OrganisationController extends BaseController {
     super.locals(req, res, (error, locals) => {
       if (error) return callback(error);
       locals.orgInfo = req.sessionModel.get('orgResults') || [];
+      locals.teams = req.sessionModel.get('teamResults') || [];
       callback(null, locals);
     });
   }
