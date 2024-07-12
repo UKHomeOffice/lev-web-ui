83a7d16b478df801b88c7c7f62b01183ef07af1e
nayhallbaqi <nayhall.baqai@digital.homeoffice.gov.uk>
2024-07-12 17:52:02 +0100

    changes for front end to fix pagination issues

--- controllers/UserManagement/OrganisationController.js
+++ controllers/UserManagement/OrganisationController.js
@@ -12,9 +12,24 @@ class OrganisationController extends BaseController {
         ...this.getOptions(req),
         url: `/admin/organisations/${req.params.id}/teams`
       });
+
+      let queryParamString = '';
+      let paramArray = [];
+      if (req.query.page) paramArray.push(`page=${req.query.page}`);
+      if (req.query.perPage) paramArray.push(`perPage=${req.query.perPage}`);
+      if (req.query.sort) paramArray.push(`sort=${req.query.sort}`);
+      if (req.query.order) paramArray.push(`order=${req.query.order}`);
+
+      for (let i = 0; i < paramArray.length; i++) {
+        if (i == 0) {
+          queryParamString = queryParamString.concat(`?${paramArray[i]}`);
+        } else {
+          queryParamString = queryParamString.concat(`&&${paramArray[i]}`);
+        }
+      }
       const userResults = await orgLookup({
         ...this.getOptions(req),
-        url: `/admin/organisations/${req.params.id}/users?page=${req.query.page}&order=${req.query.order}&direction=${req.query.direction}`
+        url: `/admin/organisations/${req.params.id}/users${queryParamString}`
       });
 
       req.sessionModel.set('orgResults', searchResults);
@@ -35,7 +50,7 @@ class OrganisationController extends BaseController {
       locals.orgInfo = req.sessionModel.get('orgResults') || [];
       locals.teams = req.sessionModel.get('teamResults') || [];
       locals.users = req.sessionModel.get('userResults') || [];
-      locals.usersMetaData = req.sessionModel.get('usersMetaData') || { userCount: 0, currentPage: 1};
+      locals.usersMetaData = req.sessionModel.get('usersMetaData') || { total: 0, currentPage: 1};
       callback(null, locals);
     });
   }
