3c3c5694cefd7ff12565d9904a5929b5eafbd81e
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2024-09-02 15:02:21 +0100

    Merged new changes into main

--- controllers/UserManagement/UserController.js
+++ controllers/UserManagement/UserController.js
@@ -25,10 +25,10 @@ class UserController extends BaseController {
       locals.userInfo = req.sessionModel.get('userResults') || [];
       locals.passwordResetAttempted = req.sessionModel.get('passwordResetAttempted') || false;
       locals.passwordResetSuccess = req.sessionModel.get('passwordResetSuccess') || false;
-      req.sessionModel.unset('passwordResetAttempted');
-      req.sessionModel.unset('passwordResetSuccess');
       locals.updatingUser = req.sessionModel.get('updatingUser') || false;
       locals.updatedUser = req.sessionModel.get('updatedUser') || false;
+      req.sessionModel.unset('passwordResetAttempted');
+      req.sessionModel.unset('passwordResetSuccess');
       req.sessionModel.unset('updatingUser');
       req.sessionModel.unset('updatedUser');
       callback(null, locals);
