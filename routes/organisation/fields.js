8840fc934dd6f614cf1a4b6b6febb6e7587fdf22
nayhallbaqi <nayhall.baqai@digital.homeoffice.gov.uk>
2024-11-07 16:08:29 +0000

    organisation create screen for lev-admin

--- routes/organisation/fields.js
+++ routes/organisation/fields.js
@@ -37,15 +37,23 @@ module.exports = {
   },
   'permissionCheckboxes': {
     type: 'checkboxes',
-    classes: "govuk-checkboxes--small",
+    classes: 'govuk-checkboxes--small',
     items: [
-      { value: "birth" },
-      { value: "death" },
-      { value: "marriage"},
-      { value: "partnership"}
+      { value: 'birth' },
+      { value: 'death' },
+      { value: 'marriage'},
+      { value: 'partnership'}
     ],
     validate: [
       'required'
+    ]
+  },
+  'organisationName': {
+    type: 'text',
+    validate: [
+      'required',
+      { type: 'regex', arguments: '^[a-zA-Z ]+$'},
+      { type: 'length', fn: (e) => e.length >= 1 && e.length <= 255 }
     ]
   }
 };
