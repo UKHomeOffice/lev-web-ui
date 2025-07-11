30fbed42e376f5a9343773c8cd4c62f6bcdc3a2d
Ashley Bickerstaff <ashley.bickerstaff@kainos.com>
2025-07-11 10:45:34 +0100

    Adding tests and fixing but with null values

--- helpers/feedbackBanner.js
+++ helpers/feedbackBanner.js
@@ -6,7 +6,7 @@ const displayFeedbackBanner = () => {
   const startDate = new Date(process.env.FEEDBACK_START);
   const endDate = new Date(process.env.FEEDBACK_END);
   const currentDate = new Date();
-  return process.env.FEEDBACK_CONTENT_HTML && currentDate >= startDate && currentDate <= endDate;
+  return !!process.env.FEEDBACK_CONTENT_HTML && currentDate >= startDate && currentDate <= endDate;
 };
 
 module.exports = displayFeedbackBanner;
