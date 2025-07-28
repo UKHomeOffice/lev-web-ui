7215715868e5f83c85bd175690be2b9e2cd64996
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2025-07-28 10:53:08 +0100

    removed debug logging

--- helpers/flsSchemaCache.js
+++ helpers/flsSchemaCache.js
@@ -14,33 +14,18 @@ module.exports.flsSchemaCache = async (req) => {
 
   try {
 
-    const userMetadata = await getUserMetadata();
-
-    console.log("USERMETADATA")
-    console.log(userMetadata)
+    const userMetadata = await getUserMetadata(req);
 
     flsSchema = await redisService.get(`flsSchema:${userMetadata.organisationId}`);
-
-    console.log("REDIS SCHEMA FETCH ")
-    console.log(flsSchema)
 
     if (flsSchema) return JSON.parse(flsSchema);
 
     const orgId = process.env.ORGANISATION_ID || userMetadata.organisationId;
 
-    console.log("ORG ID FROM METADATA")
-    console.log(orgId);
-
-    console.log("REQUEST OBJ")
-    console.log(req)
-
     const organisationInfo = await getRequest({
       ...requestOptions(req, iamApi),
       url: `/admin/organisations/${orgId}`,
     });
-
-    console.log("ORGANISATION INFO")
-    console.log(organisationInfo)
 
     flsSchema = organisationInfo.flsSchema;
     await redisService.set(`flsSchema:${userMetadata.orgId}`, JSON.stringify(flsSchema), config.fls.schemaCacheSeconds);
